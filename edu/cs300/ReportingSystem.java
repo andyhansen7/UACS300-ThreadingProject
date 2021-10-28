96f3da4254cd64b3f2eb3de3694cc2a4b965c122
Andy Hansen <arhansen@crimson.ua.edu>
2021-10-28 15:10:50 -0700

    Create runnable object

--- edu/cs300/ReportingSystem.java
+++ edu/cs300/ReportingSystem.java
@@ -5,21 +5,71 @@ import java.io.FileNotFoundException;
 import java.util.Enumeration;
 import java.util.Scanner;
 import java.util.Vector;
+import java.util.ArrayList;
 
-public class ReportingSystem {
+public class FileReaderThread implements Runnable
+{
+	/// Helper class to hold column attribute data
+	class ColumnAttributes
+	{
+		public int startValue;
+		public int endValue;
+		public String name;
+	};
 
+	/// Constructor
+	public FileReaderThread(int id, String filepath)
+	{
+		_id = id;
+		_filepath = filepath;
+	}
 
-	public ReportingSystem() {
+	/// Method called by thread
+	public void run()
+	{
+		/// Init file and scanner
+		File report= new File(_filepath);
+		Scanner reportScanner = new RecordScanner(report);
+
+		/// Load report attributes
+		String reportTitle = reportScanner.nextLine();
+		String reportSearchString = reportScanner.nextLine();
+		String outputFileName = reportScanner.nextLine();
+
+		/// Debug info
+		DebugLog.log("Thread id " + String.toString(_id) + " loaded report title: " + reportTitle);
+		DebugLog.log("Thread id " + String.toString(_id) + " loaded report search string: " + reportTitle);
+		DebugLog.log("Thread id " + String.toString(_id) + " loaded output file name: " + reportTitle);
+
+	}
+
+	string _filepath = "";
+	int _id = -1;
+}
+
+public class ReportingSystem
+{
+	public ReportingSystem()
+	{
 	  DebugLog.log("Starting Reporting System");
 	}
+
 
 	public int loadReportJobs() {
 		int reportCounter = 0;
 		try {
 
-			   File file = new File ("report_list.txt");
-
+			   File file = new File("report_list.txt");
 			   Scanner reportList = new Scanner(file);
+			   int numFiles = Integer.parseInt(reportList.nextLine());
+
+			   for(int i = 1; i <= numFiles; i++)
+			   {
+				   String fileName = reportList.nextLine();
+
+				   new Thread
+			   }
+
 
  		     //load specs and create threads for each report
 				 DebugLog.log("Load specs and create threads for each report\nStart thread to request, process and print reports");
